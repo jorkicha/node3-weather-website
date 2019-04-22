962a7b212aebed632b3e9dc6ee3e431c1f4d73af
jorkicha <jore7yy@gmail.com>
2019-04-22 17:42:05 +0300

    Remove unnecessary consol.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-//const request = require('request');
-//const request = require('request');
-
-console.log('Client side javascript file is loaded!');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
